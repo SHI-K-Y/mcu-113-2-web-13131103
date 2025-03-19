34c62ebedd58f62bcd1cd6ba24a5e26da274a3d7
Eric <eric941106@gmail.com>
2025-03-19 18:54:02 +0800

    feat(ts): 利用聯集變數紀錄產品修改日期

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,6 @@ console.log("isShow = ", isShow);
 
 let createDate = new Date();
 console.log("CreateDate = ", createDate);
+
+let modifyDate: Date | undefined = undefined;
+console.log("Modify Date = ", modifyDate);
